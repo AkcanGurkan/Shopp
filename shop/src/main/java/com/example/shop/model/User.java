16f5cc45cce18816d08dc4ca9cf6098f1f33bbe7
sh4riwsh4 <semihugur4363@gmail.com>
2024-07-12 15:04:00 +0300

    user controllerlar yazıldı

--- shop/src/main/java/com/example/shop/model/User.java
+++ shop/src/main/java/com/example/shop/model/User.java
@@ -1,4 +1,63 @@
 package com.example.shop.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String customerName;
+    private String customerSurname;
+    private String password;
+    @Column(unique = true)
+    private String username;
+    private Long phoneNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerSurname() { return customerSurname; }
+
+    public void setCustomerSurname(String customerSurname){ this.customerSurname = customerSurname; }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
 }
---
+++ shop/src/main/java/com/example/shop/service/UserService.java
@@ -0,0 +1,31 @@
+package com.example.shop.service;
+
+import com.example.shop.model.User;
+import com.example.shop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
